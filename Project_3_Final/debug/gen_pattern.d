08e0ce709e0b2827aa0cf75cfa3810f29bd08cb2
aakshajaywant <aaksha.jaywant@gmail.com>
2019-10-27 17:11:51 -0600

    Complete zip

---
+++ Project_3_Final/debug/gen_pattern.d
@@ -0,0 +1,6 @@
+debug/gen_pattern.o debug/gen_pattern.d: source/gen_pattern.c \
+ source/gen_pattern.h source/logger.h
+
+source/gen_pattern.h:
+
+source/logger.h:
---
+++ Project_3_Final/debug/main.d
@@ -0,0 +1,6 @@
+debug/main.o debug/main.d: source/main.c source/gen_pattern.h \
+ source/logger.h
+
+source/gen_pattern.h:
+
+source/logger.h:
